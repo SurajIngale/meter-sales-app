3294dda190641927783727d05bd823db3276fd39
Suraj Ingale <118906742+SurajIngale@users.noreply.github.com>
2026-03-05 00:16:28 +0530

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { AppProvider } from '@/src/context/AppContext';
-import { AppNavigator } from '@/src/navigation/AppNavigator';
+import { AppNavigator } from '@/src/navigation/AppNavigator'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <AppProvider>
       <NavigationContainer>
